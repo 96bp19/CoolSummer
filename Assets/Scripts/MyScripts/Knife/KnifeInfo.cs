15021dc5ce175b5820594a5afead4836b8fe3024
96bp19 <lanthoo170@gmail.com>
2020-05-14 15:27:44 +0545

    Basic Knife Unlocker

---
+++ Assets/Scripts/MyScripts/Knife/KnifeInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="Knife",menuName ="Knifeinfo/knife")]
+public class KnifeInfo : ScriptableObject
+{
+    public GameObject knifePrefab;
+    public Texture2D knifetexture;
+
+}
+
+[CreateAssetMenu(fileName ="usedKnifes",menuName ="Knifeinfo/usedKnife")]
+public class  UsedKnife: ScriptableObject
+{
+    public List<KnifeInfo> usedKnifes;
+}
+
+
